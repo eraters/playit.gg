322be2abf3c0baa4e5fbb5b8d5c9b9cc31b7e6ed
TheBotlyNoob <thebotlynoob@gmail.com>
2021-08-07 17:57:10 -0400

    broken rn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const fetch = require('node-fetch');
 const puppeteer = require('puppeteer');
+const spawn = require('child_process').spawn;
+const fs = require('fs');
 
 class playit {
   constructor(email = isRequired('email'), password = isRequired('password')) {
@@ -9,7 +11,10 @@ class playit {
   }
 
   async login(email = isRequired('email'), password = isRequired('password')) {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.goto(
       'https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fresponse_type%3Dtoken%26client_id%3D705634226527141919%26redirect_uri%3Dhttps%253A%252F%252Fplayit.gg%252Foauth%252Fdiscord%26scope%3Didentify'
@@ -39,6 +44,29 @@ class playit {
 
   async createTunnel({ type = 'TCP', port = 80, session = this.session }) {
     // {"id":null,"game":"custom-udp","local_port":1234,"local_ip":"127.0.0.1","local_proto":"Udp","agent_id":405688,"domain_id":null}
+  }
+
+  async claimUrl(url = isRequired('URL')) {
+    console.log(url);
+  }
+
+  async startPlayit(playitOpts = {}) {
+    playitOpts.NO_BROWSER = true;
+
+    Object.entries(playitOpts).map(([opt, value]) =>
+      fs.writeFileSync('.env', `${opt}=${value}`)
+    );
+
+    const playit = spawn(`${__dirname}/proxy.exe`, { cwd: __dirname });
+
+    playit.stderr.on('data', (data) =>
+      data.toString().match(/\bhttps:\/\/[0-9a-z\/]*/gi)
+        ? data.toString().match(/https:\/\/[0-9a-z\.\/]*/gi)[0]
+        : ''
+    );
+    playit.on('exit', (code) => {
+      console.log(`Playit Exited With Code: ${code}`);
+    });
   }
 }
 
