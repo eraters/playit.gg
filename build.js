6fc37effb7465e9002aa3f39492c064badf45256
TheBotlyNoob <thebotlynoob@gmail.com>
2021-08-22 16:21:23 -0400

    tf? [release]

--- build.js
+++ build.js
@@ -3,11 +3,14 @@ import { playit as PlayIt } from './dist/ts.js';
 
 const os = new PlayIt().os;
 
-(async () =>
+(async () => {
+  const output = `bin/${
+    os === 'win' ? 'playit.exe' : os === 'mac' ? 'playit.app' : 'playit.lin'
+  }`;
   await caxa.default({
     input: '.',
-    output: `bin/${
-      os === 'win' ? 'playit.exe' : os === 'mac' ? 'playit.app' : 'playit.lin'
-    }`,
+    output,
     command: ['{{caxa}}/node_modules/.bin/node', '{{caxa}}/test.js']
-  }))();
+  });
+  console.log(`Built PlayIt To ${output}`);
+})();
